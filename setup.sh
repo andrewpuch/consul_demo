97a06aca0e12fbadf3e70ad0f937d0765fa454eb
Andrew Puch <andrewpuch@gmail.com>
2015-09-10 15:08:32 -0400

    Update setup.sh
    Some error handling.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
+usage() {
+    echo ""
+    echo "${0} <type>"
+    echo ""
+    echo "Arguments"
+    echo "Encrypt    - [REQUIRED] This is acquired by running consul keygen."
+    echo "Private IP - [REQUIRED] The private IP address of the bootstrap server."
+    echo ""
+
+    return
+}
+
+# Check to make sure the correct number of arguments is passed.
+if [ "$#" -ne 2 ]; 
+then
+    usage
+    exit
+fi
+
 HOSTNAME=`hostname`
-PRIVATE_IP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
 ENCRYPT=$1
+PRIVATE_IP=$2
 
 sed -i -- "s/__NODE_NAME__/$HOSTNAME/g" /root/consul_demo/config.json
 sed -i -- "s/__BOOTSTRAP_PRIVATE_IP__/$PRIVATE_IP/g" /root/consul_demo/config.json
